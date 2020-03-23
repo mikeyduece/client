8c8c552418a279a6bc7fa302f7a44a93daec2433
Mike Heft <mikeheft@gmail.com>
2020-03-23 06:30:38 +0000

    Add Links to Header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,19 @@
 import React                    from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 // Components
-import StreamList from './streams/StreamList'
-import StreamCreate from './streams/StreamCreate'
-import StreamDelete from './streams/StreamDelete'
-import StreamEdit from './streams/StreamEdit'
-import StreamShow from './streams/StreamShow'
+import StreamList               from './streams/StreamList'
+import StreamCreate             from './streams/StreamCreate'
+import StreamDelete             from './streams/StreamDelete'
+import StreamEdit               from './streams/StreamEdit'
+import StreamShow               from './streams/StreamShow'
+import Header                   from './Header'
 
 const App = () => {
   return (
-    <div>
+    <div className='ui container'>
       <BrowserRouter>
         <div>
+          <Header />
           <Route exact path='/' component={ StreamList } />
           <Route path='/streams/new' component={ StreamCreate } />
           <Route path='/streams/edit' component={ StreamEdit } />
