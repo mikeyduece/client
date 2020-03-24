a550034767b2849cbbd8eef43e6e1a0a2056b26b
Mike Heft <mikeheft@gmail.com>
2020-03-24 11:08:44 +0000

    Work on form validation
    currently not showing errors

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -3,25 +3,46 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className='field'>
         <label>{ label }</label>
         <input { ...input } />
+        <div>{ meta.error }</div>
       </div>
     )
   }
 
+  onSubmit(formValues) {
+    console.log(formValues)
+  }
+
   render() {
     return (
-      <form className='ui form'>
+      <form className='ui form' onSubmit={ this.props.handleSubmit(this.onSubmit) }>
         <Field name='title' component={ this.renderInput } label='Enter Title' />
         <Field name='description' component={ this.renderInput } label='Enter Description' />
+        <button className='ui button primary'>Submit</button>
       </form>
     )
   }
 }
 
+const validate = formValues => {
+  const errors = {}
+
+  if (!formValues.title) {
+    return errors.title = 'You must enter a title'
+  }
+
+  if (!formValues.description) {
+    return errors.description = 'You must enter a description'
+  }
+
+  return errors
+}
+
 export default reduxForm({
-  form: 'streamCreate'
+  form: 'streamCreate',
+  validate
 })(StreamCreate)
