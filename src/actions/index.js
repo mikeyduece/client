a2352b89d482003b526db16c88b361e7351f6874
Mike Heft <mikeheft@gmail.com>
2020-03-25 05:19:25 +0000

    Add action creators for other stream actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-import streams               from '../apis/streams'
-import { SIGN_IN, SIGN_OUT } from './types'
+import streams                                                                                       from '../apis/streams'
+import { CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM, FETCH_STREAMS, SIGN_IN, SIGN_OUT } from './types'
 
 export const signIn = userId => {
   return {
@@ -13,5 +13,31 @@ export const signOut = () => {
 }
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues)
+  const response = await streams.post('/streams', formValues)
+
+  dispatch({ type: CREATE_STREAM, payload: response.data })
+}
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams')
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data })
+}
+
+export const fetchStream = streamId => async dispatch => {
+  const response = await streams.get(`/streams/${ streamId }`)
+
+  dispatch({ type: FETCH_STREAM, payload: response.data })
+}
+
+export const deleteStream = streamId => async dispatch => {
+  await streams.delete(`/streams/${ streamId }`)
+
+  dispatch({ type: DELETE_STREAM, payload: streamId })
+}
+
+export const editStream = (streamId, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${ streamId }`, formValues)
+
+  dispatch({ type: EDIT_STREAM, payload: response.data })
 }
