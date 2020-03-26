658a76633bdfc15cd5e865e56b196c2c8424081b
Mike Heft <mikeheft@gmail.com>
2020-03-26 12:06:12 +0000

    set up flv streaming

--- src/components/streams/StreamShow.jsx
+++ src/components/streams/StreamShow.jsx
@@ -1,11 +1,37 @@
 // Dependencies
 import React, { Component } from 'react'
+import flv from 'flv.js'
 import { connect }          from 'react-redux'
 import { fetchStream }      from '../../actions'
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props)
+
+    this.videoRef = React.createRef()
+  }
   componentDidMount() {
     this.props.fetchStream(this.props.streamId)
+    this.buildPlayer()
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer()
+  }
+
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${this.props.streamId}.flv`
+    })
+
+    this.player.attachMediaElement(this.videoRef.current)
+    this.player.load()
   }
 
   render() {
@@ -16,6 +42,7 @@ class StreamShow extends Component {
     const { title, description } = this.props.stream
     return (
       <div>
+        <video ref={this.videoRef} style={{width: '100%'}} controls />
         <h1>{ title }</h1>
         <h5>{ description }</h5>
       </div>
