7bb8aaa9581a0b77c58c4a46c3268f32afe5723e
Mike Heft <mikeheft@gmail.com>
2020-03-26 12:18:09 +0000

    Add will unmount function to destroy plyer

--- src/components/streams/StreamShow.jsx
+++ src/components/streams/StreamShow.jsx
@@ -19,6 +19,9 @@ class StreamShow extends Component {
     this.buildPlayer()
   }
 
+  componentWillUnmount() {
+    this.player.destroy()
+  }
 
   buildPlayer() {
     if (this.player || !this.props.stream) {
